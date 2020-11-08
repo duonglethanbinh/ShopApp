961d663b55e01304b853be632deef21d7a71d66b
Dương Lê Thanh Bình <binhdlt.tesse@gmail.com>
2020-11-08 19:31:18 +0700

    CategoryItemList

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import CategoryListItem from './components/CategoryListItem';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <CategoryListItem />
+      <CategoryListItem />
+      <CategoryListItem />
     </View>
   );
 }
@@ -14,8 +16,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems:"stretch",
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingLeft: 16,
+    paddingRight: 16,
   },
 });
