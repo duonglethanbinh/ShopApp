f93d679e5310f919b7e8e0a7623fd28633f2a55c
duonglethanbinh <17520279@gm.uit.edu.vn>
2020-11-24 20:39:37 +0700

    Update

--- screens/Category.js
+++ screens/Category.js
@@ -1,12 +1,58 @@
+import React from "react";
+import { StyleSheet, View, Text, FlatList } from "react-native";
+import ProductListItem from "../components/ProductListItem";
 
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+export default class Category extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: navigation.getParam("categoryName"),
+    };
+  };
 
-export default function Category() {
-    return (
-        <View>
-            <Text>Category</Text>
-        </View>
-    );
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [
+        {
+          id: 1,
+          images: [
+            {
+              url: "",
+            },
+          ],
+          name: "Robin",
+          price: "5000000",
+        },
+        {
+          id: 2,
+          images: [
+            {
+              url: "",
+            },
+          ],
+          name: "Barney",
+          price: "4000000",
+        },
+      ],
+    };
+  }
+  render() {
+    <FlatList
+      data={this.state.products}
+      numColumns={2}
+      renderItem={({ item }) => <ProductListItem product={item} />}
+      keyExtractor={(item) => `${item.id}`}
+    ></FlatList>;
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "stretch",
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    paddingLeft: 16,
+    paddingRight: 16,
+  }
+});
